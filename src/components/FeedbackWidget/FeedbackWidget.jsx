1fe8351070b4ed66ac69d0669d60cb26999f27bc
dusixx <113414935+dusixx@users.noreply.github.com>
2023-05-03 09:02:40 +0300

    adds reset btn

--- src/components/FeedbackWidget/FeedbackWidget.jsx
+++ src/components/FeedbackWidget/FeedbackWidget.jsx
@@ -1,7 +1,14 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Container, Title, OptionsList, Label } from './FeedbackWidget.styled';
 import FeedbackOptionsItem from './FeedbackOptionsItem';
+
+import {
+  Container,
+  Reset,
+  Title,
+  OptionsList,
+  Label,
+} from './FeedbackWidget.styled';
 
 const PositiveStats = ({ value }) =>
   value ? (
@@ -12,20 +19,30 @@ const PositiveStats = ({ value }) =>
     <Label>There is no feedback</Label>
   );
 
+//
+// FeedbackWidget
+//
+
 export class FeedbackWidget extends Component {
   static propTypes = {
     title: PropTypes.string,
   };
 
-  state = {
+  defaultState = {
     bad: 0,
     neutral: 0,
     good: 0,
   };
 
+  state = { ...this.defaultState };
+
   handleFeedbackLeave = e => {
     const { type } = e.currentTarget.dataset;
     this.setState(curState => ({ [type]: curState[type] + 1 }));
+  };
+
+  handleFormReset = () => {
+    this.setState({ ...this.defaultState });
   };
 
   countTotalFeedback = () =>
@@ -37,10 +54,17 @@ export class FeedbackWidget extends Component {
   };
 
   render() {
-    const { props, state, handleFeedbackLeave, countPositiveFeedback } = this;
+    const {
+      props,
+      state,
+      handleFeedbackLeave,
+      countPositiveFeedback,
+      handleFormReset,
+    } = this;
 
     return (
       <Container>
+        <Reset onClick={handleFormReset} title="reset" />
         <Title>{props.title}</Title>
         <OptionsList>
           {Object.keys(state).map(type => (
