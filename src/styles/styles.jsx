ca7b8dae00cc3ed9f9caaf9076437ab6497e12cb
dusixx <113414935+dusixx@users.noreply.github.com>
2023-05-04 02:29:20 +0300

    small fix

--- src/styles/styles.jsx
+++ src/styles/styles.jsx
@@ -2,6 +2,26 @@ import { Global, css, ThemeProvider } from '@emotion/react';
 import myTheme from './theme';
 import './index.css';
 
+const resetCSS = css`
+  p,
+  h2 {
+    padding: 0;
+    margin: 0;
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+`;
+
 const globalStyles = css`
   button {
     display: block;
@@ -12,13 +32,15 @@ const globalStyles = css`
   }
 `;
 
+const allStyles = css(resetCSS, globalStyles);
+
 const ThemeProv = ({
   theme = myTheme,
   children,
   GlobalStyles = globalStyles,
 }) => (
   <ThemeProvider theme={theme}>
-    <Global styles={globalStyles} />
+    <Global styles={allStyles} />
     {children}
   </ThemeProvider>
 );
