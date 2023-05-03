dace9f43f3e0a10a29864f79b9df4fa94d0581c9
dusixx <113414935+dusixx@users.noreply.github.com>
2023-05-04 02:43:22 +0300

    small fix

--- src/components/FeedbackWidget/FeedbackWidget.jsx
+++ src/components/FeedbackWidget/FeedbackWidget.jsx
@@ -1,4 +1,4 @@
-import { Children, Component } from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 import ResetBtn from './ResetBtn';
 import FeedbackOptions from './FeedbackOptions';
